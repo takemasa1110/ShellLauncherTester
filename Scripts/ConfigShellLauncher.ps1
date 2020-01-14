4404e8ea5a9a0c0f9acefe941ed3943f4f6451cd
Taisuke Takemasa <takemasat@lilymed.onmicrosoft.com>
2020-01-14 12:35:55 +0900

    Fixed invalid character issues

--- Scripts/ConfigShellLauncher.ps1
+++ Scripts/ConfigShellLauncher.ps1
@@ -1,28 +1,29 @@
-$AdminsSID = "S-1-5-32-544"
-$ShellLauncherClass = [wmiclass]"\\localhost\root\standardcimv2\embedded:WESL_UserSetting"
+$AdminsSID = "S-1-5-32-544"
+$ShellLauncherClass = [wmiclass]"\\localhost\root\standardcimv2\embedded:WESL_UserSetting"
 
-function Get-UsernameSID($AccountName) {
-$NTUserObject = New-Object System.Security.Principal.NTAccount($AccountName)
-$NTUserSID = $NTUserObject.Translate([System.Security.Principal.SecurityIdentifier])
-return $NTUserSID.Value
+function Get-UsernameSID($AccountName) {
+$NTUserObject = New-Object System.Security.Principal.NTAccount($AccountName)
+$NTUserSID = $NTUserObject.Translate([System.Security.Principal.SecurityIdentifier])
+return $NTUserSID.Value
 }
 
-$RestartShell = 0
-$RestartDevice = 1
-$ShutdownDevice = 2
+$RestartShell = 0
+$RestartDevice = 1
+$ShutdownDevice = 2
 $DoNothing = 3
 
 $CustomReturnCodes = @(0, 59, 1067, 13859)
 $CustomReturnCodesAction = @($ShutdownDevice, $RestartDevice, $RestartShell, $RestartDevice)
 $DefaultAction = $DoNothing
-$CustomUserSID = Get-UsernameSID(“hogehoge")
-$CustomShellPath = “C:\Programs\ShellLauncherTester\bin\Release\ShellLauncherTester.exe”
-#Reset Shell Launcher
+#Replace hogehoge by intended user name
+$CustomUserSID = Get-UsernameSID("hogehoge")
+$CustomShellPath = "C:\Programs\ShellLauncherTester\bin\Release\ShellLauncherTester.exe"
+#Reset Shell Launcher
 $ShellLauncherClass.RemoveCustomShell($AdminsSID)
 $ShellLauncherClass.RemoveCustomShell($CustomUserSID)
 $ShellLauncherClass.SetEnabled($FALSE)
-#Configure Shell Launcher
-$ShellLauncherClass.SetDefaultShell("explorer.exe", $RestartShell)
-$ShellLauncherClass.SetCustomShell($CustomUserSID, $CustomShellPath,  $CustomReturnCodes,  $CustomReturnCodesAction, $DefaultAction)
-$ShellLauncherClass.SetCustomShell($AdminsSID, "explorer.exe")
+#Configure Shell Launcher
+$ShellLauncherClass.SetDefaultShell("explorer.exe", $RestartShell)
+$ShellLauncherClass.SetCustomShell($CustomUserSID, $CustomShellPath,  $CustomReturnCodes,  $CustomReturnCodesAction, $DefaultAction)
+$ShellLauncherClass.SetCustomShell($AdminsSID, "explorer.exe")
 $ShellLauncherClass.SetEnabled($TRUE)
